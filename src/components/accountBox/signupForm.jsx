1ba78afab4b8246add07f3072a9478c2aa2420aa
midCenturyDude7 <mgriffes@gmail.com>
2022-03-14 22:09:51 -0700

    Working login app with styling and animation

---
+++ src/components/accountBox/signupForm.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BoldLink, BoxContainer, FormContainer, Input, MutedLink, SubmitButton } from "./common";
+import { Marginer } from "../marginer";
+import { AccountContext } from "./accountContext";
+
+export function SignupForm() {
+    const { switchToSignin } = React.useContext(AccountContext);
+
+    return (
+        <BoxContainer>
+            <FormContainer>
+                <Input type="text" placeholder="Full Name" />
+                <Input type="email" placeholder="Email" />
+                <Input type="password" placeholder="Password" />
+                <Input type="password" placeholder="Confirm Password" />
+            </FormContainer>
+            <Marginer direction="vertical" margin={10} />
+            <SubmitButton type="submit">Sign-up</SubmitButton>
+            <Marginer direction="vertical" margin="1em" />
+            <MutedLink href="#">
+                Already have an account? 
+                <BoldLink href="#" onClick={switchToSignin}>
+                    Signin
+                </BoldLink>
+            </MutedLink>
+        </BoxContainer>
+    );
+}
